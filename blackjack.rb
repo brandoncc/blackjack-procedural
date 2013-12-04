c7e073f1603fd2365d8d6d0372dde51b57fe7ee1
Brandon Conway <brandoncc@gmail.com>
2013-12-03 22:08:25 -0800

    initial player interaction

--- blackjack.rb
+++ blackjack.rb
@@ -30,10 +30,56 @@ def shuffle_deck
   @deck.shuffle!
 end
 
-# greet player
-# save player's name
+def say(msg)
+  puts "    #{msg}"
+end
+
+def say_with_hashrocket(msg)
+  puts "  => #{msg}"
+end
+
+while @player_name =~ /^\s*$/
+  # greet player
+  say_with_hashrocket('Hello, what is your name?')
+  # save player's name
+  @player_name = gets.chomp
+end
+
 # Ask if player knows rules. If not, show them.
-# after showing rules, wait for input to continue
+say_with_hashrocket('Do you know how to play blackjack?  If not, I can teach you!')
+
+def show_rules
+  say('')
+  say_with_hashrocket('Here is a quick overview:')
+  say('')
+  say('Objective: Accumulate as as close to 21 points as possble, without')
+  say('           going over.')
+  say('')
+  say('How this is achieved:')
+  say('  1. You and the dealer are each dealt two cards.')
+  say('  2. You are shown your cards.')
+  say('  3. You can choose to "stay" or "hit" (receive another card).')
+  say('  4. You are repeatedly given this choice, until one of the following is')
+  say('     true:')
+  say('       a: You choose to stay')
+  say('       b: You have 21 points')
+  say('       c: You go over 21 points, or "bust"')
+  say('')
+  say('Scoring:')
+  say('  Cards are scored as follows:')
+  say('    Ace: 1 or 11 points.  If 11 points makes you bust, the Ace reverts')
+  say('         to 1 point')
+  say('    2-10: scored as face value')
+  say('    Face cards (J, Q, K): 10 points each')
+  say('')
+  say('Tip: If you have an Ace in your hand which is being calculated as 11')
+  say('     points, and you are not sure whether you should hit or not, hit!')
+  say('Press any key to continue')
+
+  # after showing rules, wait for input to continue
+  gets
+end
+
 # start a game
 
 # play_game
